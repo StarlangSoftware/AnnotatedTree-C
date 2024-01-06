496393e276a9f212f7acb8a710e63012da5f50b1
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2024-01-06 11:22:04 +0300

    Updated CMakeLists.txt files and conanfile.py file for Conan 2.0 compatibility.

--- conanfile.py
+++ conanfile.py
@@ -47,7 +47,7 @@ class AnnotatedTreeConan(ConanFile):
         cmake.build()
 
     def package(self):
-        copy("*.h", keep_path=False, src=join(self.source_folder), dst=join(self.package_folder, "include"), pattern="*.h")
+        copy("*.h", keep_path=True, src=join(self.source_folder), dst=join(self.package_folder, "include"), pattern="*.h")
         copy("*.a", keep_path=False, src=self.build_folder, dst=join(self.package_folder, "lib"), pattern="*.a")
         copy("*.so", keep_path=False, src=self.build_folder, dst=join(self.package_folder, "lib"), pattern="*.so")
         copy("*.dylib", keep_path=False, src=self.build_folder, dst=join(self.package_folder, "lib"), pattern="*.dylib")
